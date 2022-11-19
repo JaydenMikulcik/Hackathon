e9fdfa592873b0f39e7484a2c3007f4373340742
JaydenMikulcik <jjmikulcik@gmail.com>
2022-11-19 10:31:19 -0700

    Added react project

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 # First Script
 
+import requests
+from bs4 import BeautifulSoup
+
+country = input("Enter a Country to search: ")
+
+data = requests.get(r"https://www.worlddata.info/{0}/{0}/index.php".format(country))
+soup = BeautifulSoup(data.text, 'html.parser')
+
+print(soup.prettify())
